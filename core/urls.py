eb3c0e0f0fadf7e830bd92bbbc9b043c27ef46be
Fábio de Sousa Leal <fsl2@msn.com>
2023-01-17 19:36:51 -0300

    IMPLEMENTA, CREATE, READ, UPDATE

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from .views import home, salvar, editar, update
 
 urlpatterns = [
-    path('', home)
+    path('', home),
+    path('salvar/', salvar, name="salvar"),
+    path('editar/<int:id>', editar, name="editar"),
+    path('update/<int:id>', update, name="update")
 ]
