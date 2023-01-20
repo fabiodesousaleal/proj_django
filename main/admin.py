fde01f799bfe0f5c6de63dde2bbb8ecdbc833e3e
fabiodesousaleal <@Nmq04711>
2023-01-20 01:11:16 -0300

    ADICIONANDO CLASS ASSUNTO E FINALIZANDO RELACIONAMENTO

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Alternativa, Questao, Disciplina
+from .models import Alternativa, Questao, Disciplina, Assunto
 
 admin.site.register(Alternativa)
 admin.site.register(Questao)
 admin.site.register(Disciplina)
+admin.site.register(Assunto)
--- main/models.py
+++ main/models.py
@@ -1,22 +1,30 @@
 from django.db import models
 
 class Disciplina(models.Model):
+    nome = models.CharField(max_length=200, null=False)
+
+    def __str__(self):
+        return self.nome
+
+class Assunto(models.Model):
     nome = models.CharField(max_length=200)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.nome
 
 class Questao(models.Model):
-    enunciado = models.TextField()
-    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    enunciado = models.TextField(null=False)
+    assunto = models.ForeignKey(Assunto, on_delete=models.CASCADE)
+    alternativa = models.ManyToManyField("Alternativa")
 
     def __str__(self):
         return self.enunciado
 
 class Alternativa(models.Model):
-    descricao = models.TextField()
+    descricao = models.TextField(null=False)
     alternativa_verdadeira = models.BooleanField(default=False)
-    questao = models.ManyToManyField("Questao")
 
     def __str__(self):
         return self.descricao
