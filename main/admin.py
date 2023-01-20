819ab267ec4acaf00a3e2ff7006798f6f8026bcc
Fábio de Sousa Leal <fsl2@msn.com>
2023-01-20 17:38:21 -0300

    ESTABELECIDO OS RELACIONAMENTOS DA CLASS QUESTÃO

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Alternativa, Questao, Disciplina, Assunto
+from .models import Questao, Disciplina, Assunto, Comentario
 
-admin.site.register(Alternativa)
 admin.site.register(Questao)
 admin.site.register(Disciplina)
 admin.site.register(Assunto)
+admin.site.register(Comentario)
+
--- main/models.py
+++ main/models.py
@@ -2,29 +2,37 @@ from django.db import models
 
 class Disciplina(models.Model):
     nome = models.CharField(max_length=200, null=False)
-
     def __str__(self):
         return self.nome
 
 class Assunto(models.Model):
     nome = models.CharField(max_length=200)
-    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
-
+    disciplina_id = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
 
 class Questao(models.Model):
     enunciado = models.TextField(null=False)
-    assunto = models.ForeignKey(Assunto, on_delete=models.CASCADE)
-    alternativa = models.ManyToManyField("Alternativa")
+    alternativa1 = models.TextField(null=False)
+    alternativa2 = models.TextField(null=True)
+    alternativa3 = models.TextField(null=True)
+    alternativa4 = models.TextField(null=True)
+    alternativa5 = models.TextField(null=True)
+    assunto_id = models.ForeignKey(Assunto, on_delete=models.CASCADE)
+    resposta = models.IntegerField(null=False)
+    nivel_dificuldade = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    data_questao_inserida = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.enunciado
 
-class Alternativa(models.Model):
-    descricao = models.TextField(null=False)
-    alternativa_verdadeira = models.BooleanField(default=False)
+class Comentario(models.Model):
+    descricao = models.TextField(null=True)
+    questao_id = models.ForeignKey(Questao, on_delete=models.CASCADE)
+    like = models.IntegerField(default=0)
 
     def __str__(self):
         return self.descricao
+
